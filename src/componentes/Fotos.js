02fd88f3fc199d8664bddf15e537bc06ec788886
Alvaro624la <alvarodepradotrabajo@gmail.com>
2022-12-11 21:39:05 +0100

    feat: visualizador de fotos en HD al clicar sobre ellas y optimizacion tiempo carga página

--- src/componentes/Fotos.js
+++ src/componentes/Fotos.js
@@ -2,32 +2,80 @@ import React, { useState } from 'react';
 
 function Fotos() {
 
-  //IMPORTAR TODAS LAS IMAGENES DE LA CARPETA '../img'
+  //IMPORTAR TODAS LAS IMAGENES DE LA CARPETA '../img/DCIM/Comprimidas_sin-perdida'
   function importAll(r) {
-    let imagenes = {};
-    r.keys().map((item, index) => { imagenes[item.replace('./', '')] = r(item); });
-    return imagenes;
+    let imagenesComp = {};
+    r.keys().map((item, index) => { imagenesComp[item.replace('./', '')] = r(item); });
+    return imagenesComp;
   }
-  const imagenes = importAll(require.context('../img/FotosJS', false, /\.(png|jpe?g|svg)$/));
-  const arr = Object.keys(imagenes);
+  const imagenesComp = importAll(require.context('../img/DCIM/Comprimidas_con-perdida', false, /\.(png|jpe?g|svg)$/i));
+  const arrImgComp = Object.keys(imagenesComp);
+
+  //IMPORTAR TODAS LAS IMAGENES DE LA CARPETA '../img/DCIM/Originales'
+  function importAll(r) {
+    let imagenesOrig = {};
+    r.keys().map((item, index) => { imagenesOrig[item.replace('./', '')] = r(item); });
+    return imagenesOrig;
+  }
+  const imagenesOrig = importAll(require.context('../img/DCIM/Originales', false, /\.(png|jpe?g|svg)$/i));
+  const arrImgOrig = Object.keys(imagenesOrig);
+
+  const [rutaActualClick, setRutaActualClick] = useState();
+
+  //Abrir/cerrar - Visualizador img originales (HD)
+  const [claseVisualizadorImgOrig, setclaseVisualizadorImgOrig] = useState('display-none');
+  const [claseBtnCerrarImgOrig, setclaseBtnCerrarImgOrig] = useState('display-none');
+  const cerrarImgOrig = () => {
+    setclaseVisualizadorImgOrig('display-none');
+    setclaseBtnCerrarImgOrig('display-none');
+  };
 
   return (
   <>
   <div className='fotos-body'>
     <div className='fotos-body__contenido'>
-      {arr.map((rutaImg, index)=>{
-        return <div className='fotos-body__contenido__div'>
+      {/*------Mostrar visualizador e img originales (HD) en DOM------*/}
+      <div className={claseVisualizadorImgOrig} onClick={cerrarImgOrig}>
+        {arrImgOrig.map((rutaImg, index)=>{
+          // const regExp1 = /(\d|\w)*\./i;
+          // const regExp2 = /(png|jpe?g|svg)$/i;
+          if(rutaActualClick == rutaImg){ //mejorar éste if para /.../i (que no importe .jpg o .JPG (includes lo he intentado y no sirve))
+            return <div 
+            key={`div__${index}`} 
+            className="fotos-body__contenido__ver-img-orig__div">
             <img 
-            key={`img_${index}`} 
+              key={`div__img__${index}`} 
+              className='fotos-body__contenido__ver-img-orig__div__img' 
+              src={imagenesOrig[rutaImg]}
+            />
+          </div>
+          }
+        })}
+        {/*------Boton placebo para cerrar visualizador------*/}
+        <div className={claseBtnCerrarImgOrig}>
+          <div className='fotos-body__contenido__ver-img-orig__btn-cerrar__bar1'></div>
+          <div className='fotos-body__contenido__ver-img-orig__btn-cerrar__bar2'></div>
+        </div>
+      </div>
+      {/*------Mostrar en DOM TODAS LAS IMAGENES DE arrImgComp------*/}
+      {arrImgComp.map((rutaImg, index)=>{
+        return <div 
+          key={`div__${index}`} 
+          className='fotos-body__contenido__div'>
+            <img 
+            key={`div__img__${index}`} 
             className='fotos-body__contenido__div__img' 
-            src={imagenes[rutaImg]}
+            src={imagenesComp[rutaImg]}
+            loading='lazy'
+            onClick={()=>{
+              setRutaActualClick(rutaImg);
+              setclaseVisualizadorImgOrig('fotos-body__contenido__ver-img-orig');
+              setclaseBtnCerrarImgOrig('fotos-body__contenido__ver-img-orig__btn-cerrar');
+            }}
           />
         </div>
       })}
     </div>
-  </div>
-  <div className=''>
-
   </div>
   </>
   )

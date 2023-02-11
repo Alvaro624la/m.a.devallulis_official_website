61add395dc22543f338688a2d0189d768892b07c
Alvaro624la <alvarodepradotrabajo@gmail.com>
2023-02-11 16:07:14 +0100

    feat: plataformas digitales icon links en "musica"
    fix: loop de las muestras de audio, desactivado

--- src/componentes/musica/MusicaMuestrasAudio.js
+++ src/componentes/musica/MusicaMuestrasAudio.js
@@ -17,122 +17,18 @@ import audio13 from '../../mp3/13. Me Duermo En Tu Recuerdo.mp3';
 import audio14 from '../../mp3/14. Mi Virgen De La Velilla.mp3';
 
 // import { FaPlay, FaPause, FaVolumeUp, FaVolumeDown, FaVolumeMute, FaShoppingCart } from "react-icons/fa";
-import { FaSpotify, FaApple, FaAmazon } from "react-icons/fa";
+import { FaSpotify, FaApple, FaAmazon, FaYoutube} from "react-icons/fa";
 import { ContextoMADV } from '../contexto/Contexto';
 
 
 function MusicaMuestrasAudio() {
     const { MADV } = useContext(ContextoMADV);
 
-    // const playAudio1 = useCallback((audio1)=>{
-    //         const newAudio1 = new Audio(audio1);
-    //         playPause ? newAudio1.pause() : newAudio1.play();
-    // }, [audio2]);
-    // const audiosArr = [];
-    // useCallback(()=>{
-    //     console.log('RENDERIZADO');
-    //     audiosArr.push(audio1, audio2, audio3, audio4, audio5, audio6, audio7, audio8, audio9, audio10, audio11, audio12, audio13);
-    // }, [audiosArr]);
-    // console.log(audiosArr);
-    // const newAudiosArr = new Array();
-    // console.log(audio1)
-    // const newAudio1 = new Audio(audio1);
-    
-    // console.log(newAudiosArr);
-    // console.log('RENDERIZADO');
-    // const a = useMemo(()=>{}, []);
-    // const newAudiosArr = new Array();
-    // useEffect(  () => {
-    //     const newAudio1 = new Audio(audio1);
-    //     const newAudio2 = new Audio(audio2);
-    //     const newAudio3 = new Audio(audio3);
-    //     const newAudio4 = new Audio(audio4);
-    //     const newAudio5 = new Audio(audio5);
-    //     const newAudio6 = new Audio(audio6);
-    //     const newAudio7 = new Audio(audio7);
-    //     const newAudio8 = new Audio(audio8);
-    //     const newAudio9 = new Audio(audio9);
-    //     const newAudio10 = new Audio(audio10);
-    //     const newAudio11 = new Audio(audio11);
-    //     const newAudio12 = new Audio(audio12);
-    //     const newAudio13 = new Audio(audio13);
-    //     newAudiosArr.push(newAudio1, newAudio2, newAudio3, newAudio4, newAudio5, newAudio6, newAudio7, newAudio8, newAudio9, newAudio10, newAudio11, newAudio12, newAudio13);
-    // },[]);
-    // console.log(newAudiosArr.length);
-
-
     const audiosArr = [audio1, audio2, audio3, audio4, audio5, audio6, audio7, audio8, audio9, audio10, audio11, audio12, audio13, audio14];
     // palabras acomnpañadas de - o ' ' | palabra unica
     const regExp = /([A-ZÁÉÍÓÚáéíóúÑñ][\wÁÉÍÓÚáéíóúÑñ]*((\s\w*)|(\s\w*))*)|([A-ZÁÉÍÓÚáéíóúÑñ][\wÁÉÍÓÚáéíóúÑñ]*)/g;
-    
 
-    // const [clasePlay, setClasePlay] = useState('display-block');
-    // const [clasePause, setClasePause] = useState('display-none');
-    // e => {
-    //     console.log(e.target.id.includes(index.toString()));
-    //     const a = e.target.id;
-    //     if(a.includes(index.toString())){
-    //     clasePlay === 'display-block' ? setClasePlay('display-none') : setClasePlay('display-block');
-    //     clasePause === 'display-block' ? setClasePause('display-none') : setClasePause('display-block');
-    //     }
-    // }
-    // const [playPause, setPlayPause] = useState(false);
     const audioaudiosArr = [];
-    // const audio = useMemo(() => new Audio(src), [src]);
-
-    // const cambiarAspecto = (e) => {
-    //     e.target.className.includes('muestra-play') 
-    //     ? e.target.className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__play-pause'
-    //     : e.target.className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__play-pause muestra-play';
-    // };
-    // const play = (e) => {
-    //     // e.target.children[0].currentSrc.play();
-    //     audio1.play();
-    //     setPlayPause(true);
-    // }
-    // const pause = (e) => {
-    //     // e.target.children[0].currentSrc.pause();
-    //     audio1.pause();
-    //     setPlayPause(false);
-    // }
-
-
-
-
-//nose si funciona o no, o si lo borro ...:
-
-
-    // const efecto = useEffect(() => { //con el useEfect carga una sola vez, entonces no creará un <audio> cada vez y podre .pause() el audio que esté reproduciendose. Pero ahora NO SE VE NADA en el DOM
-    //     const map = audiosArr.map(e=>e);
-    //     // console.warn('CARGADO-RENDERIZADO');
-    //     console.log(map);
-    //     for(let i = 0; i < map.length; i++){
-    //         const mapList = async () => {
-    //             return <div className='componente-musicamuestrasaudio-body__escuchar__song-container'>
-    //                 <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio'>
-    //                     <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__play-pause' onClick={(e)=>{
-    //                         // console.log(e);
-    //                         //                     // selectMuestra(e);
-    //                         //                     // const src = audioaudiosArr[index];
-    //                         //                     // const srcComprobacion = e.target.children[0].currentSrc;
-    //                         //                     playPause ? setPlayPause(false) : setPlayPause(true);
-    //                         //                     // playPause ? pause(src, srcComprobacion, index) : play(src, srcComprobacion, index);
-    //                     }}>
-    //                         {map[i].match(regExp).join('').replace(/[-?_?]/g, ' ')}
-    //                         {/* {console.log(map[i])} */}
-    //                         <audio 
-    //                             controls 
-    //                         >
-    //                             <source src={map[i]}></source>
-    //                         </audio>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         }
-    //         mapList();
-    //     }
-    // }, [])
-    // console.log(efecto);
 
     return (
         <>
@@ -152,67 +48,17 @@ function MusicaMuestrasAudio() {
             </div>
             <div className='componente-musicamuestrasaudio-body__escuchar'>
                 <p className='componente-musicamuestrasaudio-body__escuchar__titulo' aria-label='Titulo sección escuchar'>ESCUCHAR</p>
-                {
-
-//1. con el useEfect carga una sola vez, entonces no creará un <audio> cada vez y podre .pause() el audio que esté reproduciendose. Pero ahora NO SE VE NADA en el DOM
-
-
-                // useEffect(() => {
-                //     const map = audiosArr.map(e=>e);
-                //     // console.warn('CARGADO-RENDERIZADO');
-                //     console.log(map);
-                //     for(let i = 0; i < map.length; i++){
-                //         const mapList = async () => {
-                //             return <div className='componente-musicamuestrasaudio-body__escuchar__song-container'>
-                //                 <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio'>
-                //                     <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__play-pause' onClick={(e)=>{
-                //                         // console.log(e);
-                //                         //                     // selectMuestra(e);
-                //                         //                     // const src = audioaudiosArr[index];
-                //                         //                     // const srcComprobacion = e.target.children[0].currentSrc;
-                //                         //                     playPause ? setPlayPause(false) : setPlayPause(true);
-                //                         //                     // playPause ? pause(src, srcComprobacion, index) : play(src, srcComprobacion, index);
-                //                     }}>
-                //                         {map[i].match(regExp).join('').replace(/[-?_?]/g, ' ')}
-                //                         {/* {console.log(map[i])} */}
-                //                         <audio 
-                //                             controls 
-                //                         >
-                //                             <source src={map[i]}></source>
-                //                         </audio>
-                //                     </div>
-                //                 </div>
-                //             </div>
-                //         }
-                //         mapList();
-                //     }
-                // }, [])
-
-
-
-//2. falta que el pause() funcione, para ello lo más seguro es que reenderice cada vez que clicko un nuevo audio entonces no puedo pausar el anterior ya dado al play. Intentar ir al paso 1. pues
-
-
-                
+                {                
                 audiosArr.map((muestra, index)=>{
                     audioaudiosArr.push(new Audio(muestra));
                     return <div key={`div-n_${index}`} className='componente-musicamuestrasaudio-body__escuchar__song-container'>
                         <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio'>
-                            <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__play-pause'
-                                // onClick={(e)=>{
-                                    // selectMuestra(e);
-                                    // const src = audioaudiosArr[index];
-                                    // const srcComprobacion = e.target.children[0].currentSrc;
-                                    // playPause ? setPlayPause(false) : setPlayPause(true);
-                                    // playPause ? pause(src, srcComprobacion, index) : play(src, srcComprobacion, index);
-                                // }}
-                                >
-                                    {/* {playPause ? <FaPause aria-label='Icono de pause/pausa de audio'/> : <FaPlay aria-label='Icono de pause/pausa de audio'/>} */}
+                            <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__play-pause'>
                                     <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__play-pause__song-name'>
                                         {muestra.match(regExp).join('').replace(/[-?_?]/g, ' ')}
                                     </div>
                                     <audio
-                                    loop
+                                    // loop
                                     controls 
                                     >
                                         <source src={muestra}></source>
@@ -225,68 +71,31 @@ function MusicaMuestrasAudio() {
                             <FaSpotify className='componente-musicamuestrasaudio-body__escuchar__song-container__redes__a__final-icon'/>
                         </a>
                         <a href={MADV.appleMusic} target='_blank' rel="noreferrer noopener" aria-label='iTunes logo-link' className='componente-musicamuestrasaudio-body__escuchar__song-container__redes__a'>
-                        <FaApple className='componente-musicamuestrasaudio-body__escuchar__song-container__redes__a__final-icon'/>
+                            <FaApple className='componente-musicamuestrasaudio-body__escuchar__song-container__redes__a__final-icon'/>
                         </a>
                         <a href={MADV.amazonMusic} target='_blank' rel="noreferrer noopener" aria-label='SoundCloud logo-link' className='componente-musicamuestrasaudio-body__escuchar__song-container__redes__a'>
-                        <FaAmazon className='componente-musicamuestrasaudio-body__escuchar__song-container__redes__a__final-icon'/>
+                            <FaAmazon className='componente-musicamuestrasaudio-body__escuchar__song-container__redes__a__final-icon'/>
                         </a>
                     </div>
                 </div>
                 })
-                
-                
-                
-                
-//3. Manual porque estoy hasta ***************
                 }
-
-
-
-{/* <div className='componente-musicamuestrasaudio-body__escuchar__song-container'>
-    <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio'>
-        <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__play-pause'
-            onClick={e=>{
-                setPlayPause(!playPause);
-                cambiarAspecto(e);
-                playAudio1(audio1);
-                // playPause ? playAudio1(audio1) : playAudio1(audio1);
-                playPause ? console.log('pause') : console.log('play');
-            }
-        }>
-            {playPause ? <FaPause aria-label='Icono de pause/pausa de audio'/> : <FaPlay aria-label='Icono de pause/pausa de audio'/>}
-            {audio1.match(regExp).join('').replace(/[-?_?]/g, ' ')}
+                <div className='componente-musicamuestrasaudio-body__escuchar__links-div'>
+                    <a href={MADV.spotify} target='_blank' rel="noreferrer noopener" aria-label='Spotify logo-link' className='componente-musicamuestrasaudio-body__escuchar__links-div__a'>
+                        <FaSpotify className='componente-musicamuestrasaudio-body__escuchar__links-div__a__icon'/>
+                    </a>
+                    <a href={MADV.youtube} target='_blank' rel="noreferrer noopener" aria-label='Youtube logo-link' className='componente-musicamuestrasaudio-body__escuchar__links-div__a'>
+                        <FaYoutube className='componente-musicamuestrasaudio-body__escuchar__links-div__a__icon'/>
+                    </a>
+                    <a href={MADV.appleMusic} target='_blank' rel="noreferrer noopener" aria-label='iTunes logo-link' className='componente-musicamuestrasaudio-body__escuchar__links-div__a'>
+                        <FaApple className='componente-musicamuestrasaudio-body__escuchar__links-div__a__icon'/>
+                    </a>
+                    <a href={MADV.amazonMusic} target='_blank' rel="noreferrer noopener" aria-label='SoundCloud logo-link' className='componente-musicamuestrasaudio-body__escuchar__links-div__a'>
+                        <FaAmazon className='componente-musicamuestrasaudio-body__escuchar__links-div__a__icon'/>
+                    </a>
+                </div>
             </div>
-            <div className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__redes'>
-            <a href='#' target='_blank' rel="noreferrer" aria-label='Spotify logo-link' className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__redes__a'>
-                <FaSpotify className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__redes__a__final-icon'/>
-            </a>
-            <a href='#' target='_blank' rel="noreferrer" aria-label='iTunes logo-link' className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__redes__a'>
-            <FaItunes className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__redes__a__final-icon'/>
-            </a>
-            <a href='#' target='_blank' rel="noreferrer" aria-label='SoundCloud logo-link' className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__redes__a'>
-            <FaSoundcloud className='componente-musicamuestrasaudio-body__escuchar__song-container__reproductor-audio__redes__a__final-icon'/>
-            </a>
         </div>
-    </div>
-</div>
-
-<div>{audio2.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio3.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio4.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio5.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio6.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio7.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio8.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio9.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio10.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio11.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio12.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div>
-<div>{audio13.match(regExp).join('').replace(/[-?_?]/g, ' ')}</div> */}
-
-
-        
-        </div>
-    </div>
     </>
     )
 }

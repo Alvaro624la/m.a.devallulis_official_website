33ceaa93ccbfdde11e59d2cb2d88990353843c59
Alvaro624la <alvarodepradotrabajo@gmail.com>
2023-03-25 20:10:10 +0100

    Fix: Fotos --> Cambio total de código
    En Vite no soy capaz de obtener las rutas compiladas de las imágenes
    (como hacía antes de migrar con el require.context del Webpack).
    Entonces añado las IMG medio manual en vez completamente automático
    como antes.

--- src/componentes/Fotos.jsx
+++ src/componentes/Fotos.jsx
@@ -1,18 +1,48 @@
 import React, { useState } from 'react';
 import { Helmet } from "react-helmet";
+import lowIMG_3852 from '../img/DCIM/Poca-calidad/IMG_3852.jpg';
+import HDIMG_3852 from '../img/DCIM/HD/IMG_3852.jpg';
+// import lowIMG_3857 from '../img/DCIM/Poca-calidad/IMG_3857.jpg';
+// import HDIMG_3857 from '../img/DCIM/HD/IMG_3857.jpg';
+// import lowIMG_3841 from '../img/DCIM/Poca-calidad/IMG_3841.jpg';
+// import HDIMG_3841 from '../img/DCIM/HD/IMG_3841.jpg';
+// import lowIMG_3982 from '../img/DCIM/Poca-calidad/IMG_3982.jpg';
+// import HDIMG_3982 from '../img/DCIM/HD/IMG_3982.jpg';
+import lowIMG_4007 from '../img/DCIM/Poca-calidad/IMG_4007.jpg';
+import HDIMG_4007 from '../img/DCIM/HD/IMG_4007.jpg';
+import lowIMG_4063 from '../img/DCIM/Poca-calidad/IMG_4063.jpg';
+import HDIMG_4063 from '../img/DCIM/HD/IMG_4063.jpg';
+import lowIMG_4109 from '../img/DCIM/Poca-calidad/IMG_4109.jpg';
+import HDIMG_4109 from '../img/DCIM/HD/IMG_4109.jpg';
+import lowIMG_4114 from '../img/DCIM/Poca-calidad/IMG_4114.jpg';
+import HDIMG_4114 from '../img/DCIM/HD/IMG_4114.jpg';
+import lowIMG_4180 from '../img/DCIM/Poca-calidad/IMG_4180.jpg';
+import HDIMG_4180 from '../img/DCIM/HD/IMG_4180.jpg';
 
 function Fotos() {
-  // console.log('fotos cargado');
-  //IMPORTAR TODAS LAS IMAGENES DE LA CARPETA '../img/DCIM/Comprimidas_sin-perdida'
-  const imagenesComp = import.meta.glob("../img/DCIM/Poca-calidad/*.jpg");
-  const arrImgComp = Object.keys(imagenesComp);
-  // console.log(arrImgComp);
-  //////
-  //IMPORTAR TODAS LAS IMAGENES DE LA CARPETA '../img/DCIM/Originales'
-  const imagenesOrig = import.meta.glob("../img/DCIM/HD/*.jpg");
-  const arrImgOrig = Object.keys(imagenesOrig);
-  // console.log(arrImgOrig);
-  //////
+  const arrLowImg = [
+    lowIMG_3852,
+    // lowIMG_3857,
+    //  lowIMG_3841,
+    // lowIMG_3982,
+    lowIMG_4007,
+    lowIMG_4063,
+    lowIMG_4109,
+    lowIMG_4114,
+    lowIMG_4180
+    ];
+  const arrHDImg = [
+    HDIMG_3852,
+    // HDIMG_3857,
+    // HDIMG_3841,
+    // HDIMG_3982,
+    HDIMG_4007,
+    HDIMG_4063,
+    HDIMG_4109,
+    HDIMG_4114,
+    HDIMG_4180
+  ];
+
   const [rutaActualClick, setRutaActualClick] = useState('');
 
   //Abrir/cerrar - Visualizador img originales (HD)
@@ -23,82 +53,66 @@ function Fotos() {
     setclaseBtnCerrarImgOrig('display-none');
   };
   //////
-  // ------ CREAR POLYFILL ------ //
-  // ------ DESACTIVAR ERROR DE ESLINT PARA CAMBIAR FUNCIONES NATIVAS, CON LA SIGUIENTE FRASE COMENTADA: ------ //
-  // eslint-disable-next-line no-extend-native
-  Array.prototype.alvaroMap = function(callback){
-    let result = [];
-    for(let index = 0; index < this.length; index++){
-      const el = this[index];
-      const elTransformado = callback(el, index);
-      result.push(elTransformado);
-    }
-    return result;
-  }
-  // COMPROBANDO EL POLYFILL:
-  // const a = [2, 3];
-  // console.log(a.map(callback => callback + 10));
-  // console.log(a.alvaroMap(callback => callback + 10));
-  //////
+
   return (
-  <>
-  <Helmet>
-    <title>Fotos | Miguel Ángel de Vallulis</title>
-    <meta name="description" content="Aquí verás unas cuantas fotos de Miguel Ángel" />
-  </Helmet>
-  <div className='fotos-body'>
-    <div className='fotos-body__contenido'>
-      {/*------Mostrar visualizador e img originales (HD) en DOM------*/}
-      <div className={claseVisualizadorImgOrig} onClick={cerrarImgOrig}>
-        {
-          arrImgOrig.alvaroMap((rutaImg, index)=>{
-            if(rutaActualClick.includes(rutaImg.match(/IMG_\d{4}/g))){ // se entiende que es (... === true)
-              // console.log('Con esta casa');
-              return <div 
-              key={`div__${index}`} 
-              className="fotos-body__contenido__ver-img-orig__div">
+    <>
+    <Helmet>
+      <title>Fotos | Miguel Ángel de Vallulis</title>
+      <meta name="description" content="Aquí verás unas cuantas fotos de Miguel Ángel" />
+    </Helmet>
+    <div className='fotos-body'>
+      <div className='fotos-body__contenido'>
+      {/* ------ Mostrar en DOM TODAS LAS IMAGENES Low DE arrLowImg ------ */}
+      {
+        arrLowImg.map((img, i)=>{
+          return <div 
+            key={`div__${i}`} 
+            className='fotos-body__contenido__div'>
               <img 
-                key={`div__img__${index}`} 
-                className='fotos-body__contenido__ver-img-orig__div__img' 
-                aria-label={`Imagen nombrada como: ${rutaImg.replace('../', 'src/')}`} 
-                src={rutaImg.replace('../', 'src/')}
-              />
-            </div>
-            } 
-            // else {console.log('Con esta no casa')}
-          })
-        }
-        {/*------Boton placebo para cerrar visualizador------*/}
-        <div className={claseBtnCerrarImgOrig}>
-          <div className='fotos-body__contenido__ver-img-orig__btn-cerrar__bar1'></div>
-          <div className='fotos-body__contenido__ver-img-orig__btn-cerrar__bar2'></div>
+              key={`div__img__${i}`} 
+              className='fotos-body__contenido__div__img' 
+              aria-label={`Imagen nombrada como: ${img}`} 
+              src={img}
+              loading='lazy'
+              onClick={()=>{
+                setRutaActualClick(img);
+                setclaseVisualizadorImgOrig('fotos-body__contenido__ver-img-orig');
+                setclaseBtnCerrarImgOrig('fotos-body__contenido__ver-img-orig__btn-cerrar');
+              }}
+            />
+          </div>
+        })
+      }
+      {/*------ Mostrar visualizador de img HD en DOM de arrImgOrig ------*/}
+        <div className={claseVisualizadorImgOrig} onClick={cerrarImgOrig}>
+          {
+            arrHDImg.map((img, i)=>{
+              if(rutaActualClick.includes(img.match(/IMG_\d{4}/g))){ // se entiende que es (... === true)
+                return <div 
+                key={`div__${i}`} 
+                className="fotos-body__contenido__ver-img-orig__div">
+                <img 
+                  key={`div__img__${i}`} 
+                  className='fotos-body__contenido__ver-img-orig__div__img' 
+                  aria-label={`Imagen nombrada como: ${img}`} 
+                  src={img}
+                />
+              </div>
+              } 
+            })
+          }
+          {/*------ Boton placebo para cerrar visualizador ------*/}
+          <div className={claseBtnCerrarImgOrig}>
+            <div className='fotos-body__contenido__ver-img-orig__btn-cerrar__bar1'></div>
+            <div className='fotos-body__contenido__ver-img-orig__btn-cerrar__bar2'></div>
+          </div>
         </div>
       </div>
-      {/*------Mostrar en DOM TODAS LAS IMAGENES DE arrImgComp------*/}
-      {arrImgComp.alvaroMap((rutaImg, index)=>{
-        return <div 
-          key={`div__${index}`} 
-          className='fotos-body__contenido__div'>
-            <img 
-            key={`div__img__${index}`} 
-            className='fotos-body__contenido__div__img' 
-            aria-label={`Imagen nombrada como: ${rutaImg}`} 
-            src={arrImgComp[index].replace('../', 'src/')}
-            loading='lazy'
-            onClick={()=>{
-              setRutaActualClick(rutaImg);
-              setclaseVisualizadorImgOrig('fotos-body__contenido__ver-img-orig');
-              setclaseBtnCerrarImgOrig('fotos-body__contenido__ver-img-orig__btn-cerrar');
-            }}
-          />
-        </div>
-      })}
     </div>
-  </div>
-  </>
-  )
-}
-
-export default React.memo(Fotos);
-
-//  Line 44:41:  Array.prototype.map() expects a value to be returned at the end of arrow function  array-callback-return
+    </>
+    )
+  }
+  
+  export default React.memo(Fotos);
+  
+  //  Line 44:41:  Array.prototype.map() expects a value to be returned at the end of arrow function  array-callback-return
